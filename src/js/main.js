b902377e7f4f36c89bb495b8e58a718f36277438
TafaniM <marina.tafani@studentfr.ch>
2023-11-23 13:41:52 +0100

    16-modification carte

--- src/js/main.js
+++ src/js/main.js
@@ -8,26 +8,27 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
 
 function init() {
-
     let tl = gsap.timeline();
     // animate .ci
-    tl.to('.circle', {
+    tl.to(".circle", {
         motionPath: {
-            path: '#map-svg path',
-            align: '#map-svg path',
+            path: "#map-svg path",
+            align: "#map-svg path",
             alignOrigin: [0.5, 0.5],
             // start: 0,
             // end: 0.3333,
         },
         scrollTrigger: {
-            trigger: ".test-map",
+            trigger: ".map-container",
             start: "top top",
-            end: "+=1000px", // Nombre de pixels scrollés pour produire l'animation
+            end: "100%", // Nombre de pixels scrollés pour produire l'animation
             scrub: 1,
             pin: true,
             markers: true,
+            fastScrollEnd: true,
+            preventOverlaps: false,
         },
-        ease: "none",
+        ease: "power1.inOut",
         // duration: 20
     });
 }
@@ -35,64 +36,62 @@ function init() {
 gsap.ticker.add(() =>
     gsap.to(".test-map", {
         duration: 0.5,
-        x: -gsap.getProperty(".circle", "x") * 0.5,
-        y: -gsap.getProperty(".circle", "y") * 0.5,
+        x: -gsap.getProperty(".circle", "x") * 0.65,
+        y: -gsap.getProperty(".circle", "y") * 0.65,
     })
 );
 
-// window.onload = window.onresize = () => {
-//     gsap.set(".test-map", {
-//         left: window.innerWidth / 2,
-//         top: window.innerHeight / 2
-//     });
-// };
+window.onload = window.onresize = () => {
+    gsap.set(".map-container", {
+        // left: window.innerWidth / 2,
+        // top: window.innerHeight / 2,
+    });
+};
 
 document.addEventListener("DOMContentLoaded", init);
 
-
-
 // overlay 1
 
-var html = document.querySelector('html');
-var story1 = document.querySelector('#button1');
-var overlay1 = document.querySelector('#overlay1');
+var html = document.querySelector("html");
+var story1 = document.querySelector("#button1");
+var overlay1 = document.querySelector("#overlay1");
 
-story1.addEventListener('click', function () {
-    overlay1.classList.remove('overlay-hidden');
-    html.classList.add('scroll-lock');
-})
+story1.addEventListener("click", function () {
+    overlay1.classList.remove("overlay-hidden");
+    html.classList.add("scroll-lock");
+});
 
-overlay1.addEventListener('click', function () {
-    overlay1.classList.add('overlay-hidden');
-    html.classList.remove('scroll-lock');
-})
+overlay1.addEventListener("click", function () {
+    overlay1.classList.add("overlay-hidden");
+    html.classList.remove("scroll-lock");
+});
 
 // overlay 2
 
-var story2 = document.querySelector('#button2');
-var overlay2 = document.querySelector('#overlay2');
+var story2 = document.querySelector("#button2");
+var overlay2 = document.querySelector("#overlay2");
 
-story2.addEventListener('click', function () {
-    overlay2.classList.remove('overlay-hidden');
-    html.classList.add('scroll-lock');
-})
+story2.addEventListener("click", function () {
+    overlay2.classList.remove("overlay-hidden");
+    html.classList.add("scroll-lock");
+});
 
-overlay2.addEventListener('click', function () {
-    overlay2.classList.add('overlay-hidden');
-    html.classList.remove('scroll-lock');
-})
+overlay2.addEventListener("click", function () {
+    overlay2.classList.add("overlay-hidden");
+    html.classList.remove("scroll-lock");
+});
 
 // overlay 3
 
-var story3 = document.querySelector('#button3');
-var overlay3 = document.querySelector('#overlay3');
+var story3 = document.querySelector("#button3");
+var overlay3 = document.querySelector("#overlay3");
 
-story3.addEventListener('click', function () {
-    overlay3.classList.remove('overlay-hidden');
-    html.classList.add('scroll-lock');
-})
+story3.addEventListener("click", function () {
+    overlay3.classList.remove("overlay-hidden");
+    html.classList.add("scroll-lock");
+});
 
-overlay3.addEventListener('click', function () {
-    overlay3.classList.add('overlay-hidden');
-    html.classList.remove('scroll-lock');
-})
+overlay3.addEventListener("click", function () {
+    overlay3.classList.add("overlay-hidden");
+    html.classList.remove("scroll-lock");
+});
